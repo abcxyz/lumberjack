c4d615faeba7a8679c128a346e3e251c6660faf2
Chris Wilcox <crwilcox@google.com>
2022-01-11 16:16:23 -0800

    logging

--- scripts/integration_build.sh
+++ scripts/integration_build.sh
@@ -24,9 +24,12 @@ GO_BUILD_COMMAND=${ROOT}/clients/go/test/shell/build.sh
 JAVA_BUILD_COMMAND=${ROOT}/clients/java-logger/scripts/build_shell.sh
 
 export TF_LOG=TRACE
-echo "PRE TERRAFORM"
+echo `gcloud config get-value project`
+echo `gcloud auth list`
+
+echo "TERRAFORM INIT"
 terraform -chdir=${TF_ENVS_CI_DIR} init
-echo "PRE APPLY"
+echo "TERRAFORM APPLY"
 terraform -chdir=${TF_ENVS_CI_DIR} apply -auto-approve
 
 SHELL_APP_PROJECT_ID=$(terraform -chdir=${TF_ENVS_CI_DIR} output -raw app_project)
