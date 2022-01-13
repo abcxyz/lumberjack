7ab181e21af1b5694792810f23ed565119466a22
Mete Yurtoglu <myurtoglu@gmail.com>
2022-01-13 15:13:42 -0800

    fix: Add service account filtering to Filtering processor by default.
    With this change, by default, logs from service accounts will be filtered out when no explicit include/exclude filter patterns are provided.

---
+++ clients/java-logger/library/src/test/java/com/abcxyz/lumberjack/auditlogclient/modules/FilteringProcessorModuleTest.java
@@ -0,0 +1,40 @@
+package com.abcxyz.lumberjack.auditlogclient.modules;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.abcxyz.lumberjack.auditlogclient.processor.FilteringProcessor;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.junit.jupiter.api.Test;
+
+class FilteringProcessorModuleTest {
+  private static Injector injector() {
+    return Guice.createInjector(new FilteringProcessorModule());
+  }
+
+  @Test
+  public void providesFilteringProcessor() {
+    assertThat(injector().getInstance(FilteringProcessor.class)).isNotNull();
+  }
+
+  @Test
+  public void providedFilteringProcessorExcludesServiceAccountsByDefault() {
+    FilteringProcessor filteringProcessor = injector().getInstance(FilteringProcessor.class);
+    assertThat(filteringProcessor.getExcludePatterns().size()).isEqualTo(1);
+    assertThat(
+            filteringProcessor
+                .getExcludePatterns()
+                .get(0)
+                .matcher("abc@project.iam.gserviceaccount.com")
+                .matches())
+        .isTrue();
+    assertThat(filteringProcessor.getExcludePatterns().get(0).matcher("foo@bar.com").matches())
+        .isFalse();
+  }
+
+  @Test
+  public void providedFilteringProcessorDoesNotHaveIncludeFilterSetByDefault() {
+    FilteringProcessor filteringProcessor = injector().getInstance(FilteringProcessor.class);
+    assertThat(filteringProcessor.getIncludePatterns()).isEmpty();
+  }
+}
