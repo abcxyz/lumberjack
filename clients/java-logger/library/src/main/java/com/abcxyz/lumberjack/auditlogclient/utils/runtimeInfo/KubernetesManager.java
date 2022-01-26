099bbc41a6ae1ae2c924c212a9725d060d6dc0b2
Mayuri <mayurinaware@gmail.com>
2022-01-26 07:10:47 +0530

    Java: Fixing/refactoring the runtimeInfo processor (#77)
    * Fixing the runtimeInfo with system varibles rather than environment vars and refactoring to provider class
    
    * adding provider file
    
    * refactor to have manager for each GCP service and its equivalent tests
    
    * Added resource link on how to detect the env
    
    * minor comment/nit fix
    
    Co-authored-by: Mayuri Naware <mayurinaware@google.com>
    Co-authored-by: Mete Yurtoglu <myurtoglu@gmail.com>

---
+++ clients/java-logger/library/src/main/java/com/abcxyz/lumberjack/auditlogclient/utils/runtimeInfo/KubernetesManager.java
@@ -0,0 +1,45 @@
+package com.abcxyz.lumberjack.auditlogclient.utils.runtimeInfo;
+
+import com.google.api.MonitoredResource;
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import java.io.IOException;
+
+/**
+ * KubernetesManager provides functionality for getting run time info for processes running on
+ * GKE.
+ */
+public class KubernetesManager {
+
+  private final String hostname;
+  private final String containerName;
+  private final RuntimeInfoCommonUtils runtimeInfoCommonUtils;
+
+  @Inject
+  public KubernetesManager(@Named("HOSTNAME") final String hostname,
+      @Named("CONTAINER_NAME") final String containerName,
+      RuntimeInfoCommonUtils runtimeInfoCommonUtils) {
+    this.hostname = hostname;
+    this.containerName = containerName;
+    this.runtimeInfoCommonUtils = runtimeInfoCommonUtils;
+  }
+
+  public MonitoredResource detectKubernetesResource() throws IOException {
+    return MonitoredResource.newBuilder()
+        .setType("k8s_container")
+        .putLabels("project_id", runtimeInfoCommonUtils.getProjectId())
+        .putLabels("cluster_name", runtimeInfoCommonUtils.getClusterName())
+        .putLabels("pod_name", hostname)
+        .putLabels("container_name", containerName)
+        .putLabels("namespace_name", runtimeInfoCommonUtils.getNamespace())
+        .putLabels("location", runtimeInfoCommonUtils.getZone())
+        .build();
+  }
+
+  public boolean isKubernetesEngine() {
+    String clusterName = runtimeInfoCommonUtils.getClusterName();
+    return clusterName != null && !clusterName.isBlank();
+  }
+
+
+}
