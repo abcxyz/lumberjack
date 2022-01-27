fc382bf15547a53b5cfee0fff4e51f829e702640
Ryan Serva <rsrv@google.com>
2022-01-27 15:59:25 -0800

    adding big query support to grpc integ test

--- clients/java-logger/grpc-test-app/src/main/java/abcxyz/lumberjack/test/talker/TalkerClient.java
+++ clients/java-logger/grpc-test-app/src/main/java/abcxyz/lumberjack/test/talker/TalkerClient.java
@@ -38,6 +38,13 @@ import com.abcxyz.lumberjack.test.talker.WhisperRequest;
 import com.abcxyz.lumberjack.test.talker.WhisperResponse;
 import com.google.auth.oauth2.AccessToken;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.bigquery.Job;
+import com.google.cloud.bigquery.JobId;
+import com.google.cloud.bigquery.JobInfo;
+import com.google.cloud.bigquery.QueryJobConfiguration;
+import com.google.cloud.bigquery.TableResult;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
@@ -48,12 +55,16 @@ import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.StreamSupport;
 
 /** A simple client that requests a greeting from the {@link TalkerService} with TLS. */
 public class TalkerClient {
   private static final Logger logger = Logger.getLogger(TalkerClient.class.getName());
-
   private final TalkerGrpc.TalkerBlockingStub blockingStub;
+
+  private static final int INITIAL_AUDIT_LOG_WAIT_SECONDS = 60;
+  private static final int RETRY_DELAY_SECONDS = 10;
+  private static final int RETRY_COUNT = 5;
 
   /** Construct client for accessing {@link TalkerService} using the existing channel. */
   public TalkerClient(ManagedChannel channel, GoogleCredentials credentials) throws IOException {
@@ -65,10 +76,8 @@ public class TalkerClient {
   /** Say hello to server. */
   public void greet(String name, UUID target) {
     logger.info("Will try to greet " + name + " ...");
-    HelloRequest request = HelloRequest.newBuilder()
-        .setMessage(name)
-        .setTarget(target.toString())
-        .build();
+    HelloRequest request =
+        HelloRequest.newBuilder().setMessage(name).setTarget(target.toString()).build();
     HelloResponse response;
     try {
       response = blockingStub.hello(request);
@@ -82,11 +91,8 @@ public class TalkerClient {
   /** Whisper secrets to server. */
   public void whisper(String secret, UUID target) {
     logger.info("Will try to whisper " + secret + " ...");
-    WhisperRequest request = WhisperRequest
-        .newBuilder()
-        .setMessage(secret)
-        .setTarget(target.toString())
-        .build();
+    WhisperRequest request =
+        WhisperRequest.newBuilder().setMessage(secret).setTarget(target.toString()).build();
     WhisperResponse response;
     try {
       response = blockingStub.whisper(request);
@@ -129,6 +135,11 @@ public class TalkerClient {
       credentials = GoogleCredentials.getApplicationDefault();
     }
 
+    String projectId = System.getenv().get("BACKEND_PROJECT_ID");
+    String datasetQuery = System.getenv().get("BIGQUERY_DATASET_QUERY");
+
+    BigQuery bigquery = BigQueryOptions.newBuilder().setProjectId(projectId).build().getService();
+
     for (String host : hosts) {
       ManagedChannel channel = ManagedChannelBuilder.forAddress(host, port).build();
 
@@ -136,10 +147,46 @@ public class TalkerClient {
         TalkerClient client = new TalkerClient(channel, credentials);
         UUID target = UUID.randomUUID();
         client.greet(host, target);
-        client.whisper("This is a secret! Don't audit log this string", target);
+        QueryJobConfiguration queryConfig = createQuery(projectId, datasetQuery, target.toString());
+
+        UUID target2 = UUID.randomUUID();
+        client.whisper("This is a secret! Don't audit log this string", target2);
+        QueryJobConfiguration queryConfig2 =
+            createQuery(projectId, datasetQuery, target2.toString());
+
+        logger.info("Sleeping and waiting for logs to propogate...");
+        TimeUnit.SECONDS.sleep(INITIAL_AUDIT_LOG_WAIT_SECONDS);
+        validate(queryConfig, bigquery);
+        validate(queryConfig2, bigquery);
       } finally {
         channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
       }
     }
+  }
+
+  private static QueryJobConfiguration createQuery(
+      String project, String datasetQuery, String target) {
+    String query =
+        String.format(
+            "SELECT * FROM %s.%s WHERE jsonPayload.resource_name=\"%s\" LIMIT 1",
+            project, datasetQuery, target);
+    return QueryJobConfiguration.newBuilder(query).setUseLegacySql(false).build();
+  }
+
+  private static void validate(QueryJobConfiguration queryConfig, BigQuery bigQuery)
+      throws InterruptedException {
+    JobId jobId = JobId.of(UUID.randomUUID().toString());
+    Job queryJob = bigQuery.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
+
+    for (int i = 0; i < RETRY_COUNT; i++) {
+      TableResult result = queryJob.getQueryResults();
+      if (StreamSupport.stream(result.getValues().spliterator(), false).count() == 1) {
+        logger.info("Found a matching audit log!");
+        return;
+      }
+      logger.info("Not found, will retry in " + RETRY_DELAY_SECONDS + " seconds...");
+      TimeUnit.SECONDS.sleep(RETRY_DELAY_SECONDS);
+    }
+    throw new RuntimeException("Unable to find matching audit log in BigQuery.");
   }
 }
