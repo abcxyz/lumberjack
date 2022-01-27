42ba86f59b6fbc8dbb6994050f7f7a4743b5db7e
Ryan Serva <rsrv@google.com>
2022-01-27 13:10:33 -0800

    Feat: added additional validations for ValidationProcessor

--- clients/java-logger/library/src/main/java/com/abcxyz/lumberjack/auditlogclient/processor/ValidationProcessor.java
+++ clients/java-logger/library/src/main/java/com/abcxyz/lumberjack/auditlogclient/processor/ValidationProcessor.java
@@ -18,6 +18,7 @@ package com.abcxyz.lumberjack.auditlogclient.processor;
 
 import com.abcxyz.lumberjack.auditlogclient.processor.LogProcessor.LogValidator;
 import com.abcxyz.lumberjack.v1alpha1.AuditLogRequest;
+import com.google.api.client.util.Preconditions;
 import com.google.inject.Inject;
 import lombok.AllArgsConstructor;
 
@@ -28,13 +29,25 @@ public class ValidationProcessor implements LogValidator {
   /** Validates the given {@link AuditLogRequest} */
   @Override
   public AuditLogRequest process(AuditLogRequest auditLogRequest) throws IllegalArgumentException {
-
-    if (auditLogRequest == null) {
-      throw new IllegalArgumentException("Input auditLogRequest is null");
-    }
-    if (!auditLogRequest.hasPayload()) {
-      throw new IllegalArgumentException("Input auditLogRequest does not have payload");
-    }
+    Preconditions.checkArgument(auditLogRequest != null, "Input auditLogRequest is null");
+    Preconditions.checkArgument(
+        auditLogRequest.hasPayload(), "Input auditLogRequest does not have payload");
+    Preconditions.checkArgument(
+        auditLogRequest.getPayload().hasAuthenticationInfo(),
+        "Input auditLogRequest does not have authentication info");
+    Preconditions.checkArgument(
+        auditLogRequest.getPayload().getResourceName() != null
+            && !auditLogRequest.getPayload().getResourceName().isBlank(),
+        "Input auditLogRequest does not have resource name");
+    Preconditions.checkArgument(
+        auditLogRequest.getPayload().getServiceName() != null
+            && !auditLogRequest.getPayload().getServiceName().isBlank(),
+        "Input auditLogRequest does not have service name");
+    Preconditions.checkArgument(
+        auditLogRequest.getPayload().getMethodName() != null
+            && !auditLogRequest.getPayload().getMethodName().isBlank(),
+        "Input auditLogRequest does not have method name");
+    // timestamp?
     return auditLogRequest;
   }
 }
