8eaa93ea16367a8dd500e6695351e28ec1b36b0e
Chen Shou <cshou@google.com>
2022-02-22 19:19:38 +0000

    test: Enable Go gRPC stream handling integ tests

--- clients/go/test/grpc-app/main.go
+++ clients/go/test/grpc-app/main.go
@@ -17,11 +17,14 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 
 	"github.com/abcxyz/lumberjack/clients/go/pkg/audit"
 	"github.com/abcxyz/lumberjack/clients/go/pkg/auditopt"
@@ -86,4 +89,94 @@ func (s *server) Bye(ctx context.Context, req *talkerpb.ByeRequest) (*talkerpb.B
 	return &talkerpb.ByeResponse{
 		Message: "Bye!",
 	}, nil
+}
+
+func (s *server) Fibonacci(req *talkerpb.FibonacciRequest, svr talkerpb.Talker_FibonacciServer) error {
+	if logReq, ok := audit.LogReqFromCtx(svr.Context()); ok {
+		logReq.Payload.ResourceName = req.Target
+	}
+
+	p := req.Places
+	svr.Send(&talkerpb.FibonacciResponse{
+		Position: 1,
+		Value:    0,
+	})
+	if p > 1 {
+		svr.Send(&talkerpb.FibonacciResponse{
+			Position: 2,
+			Value:    1,
+		})
+	}
+
+	var x, y uint32 = 0, 1
+	for i := uint32(3); i <= p; i++ {
+		z := x + y
+		x = y
+		y = z
+		svr.Send(&talkerpb.FibonacciResponse{
+			Position: i,
+			Value:    z,
+		})
+	}
+
+	return nil
+}
+
+func (s *server) Addition(svr talkerpb.Talker_AdditionServer) error {
+	sum := 0
+
+	for {
+		req, err := svr.Recv()
+		if err == io.EOF {
+			// End of stream. Send the sum.
+			if err := svr.SendAndClose(&talkerpb.AdditionResponse{
+				Sum: uint64(sum),
+			}); err != nil {
+				return err
+			}
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		if logReq, ok := audit.LogReqFromCtx(svr.Context()); ok {
+			logReq.Payload.ResourceName = req.Target
+		}
+		sum += int(req.Addend)
+	}
+
+	return nil
+}
+
+func (s *server) Fail(ctx context.Context, req *talkerpb.FailRequest) (*talkerpb.FailResponse, error) {
+	if logReq, ok := audit.LogReqFromCtx(ctx); ok {
+		logReq.Payload.ResourceName = req.Target
+	}
+	return nil, status.Errorf(codes.ResourceExhausted, "this call will always fail")
+}
+
+func (s *server) FailOnFour(svr talkerpb.Talker_FailOnFourServer) error {
+	for {
+		req, err := svr.Recv()
+		if err == io.EOF {
+			// End of stream. Send the sum.
+			svr.SendAndClose(&talkerpb.FailOnFourResponse{
+				Message: "closing...",
+			})
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		if logReq, ok := audit.LogReqFromCtx(svr.Context()); ok {
+			logReq.Payload.ResourceName = req.Target
+		}
+
+		if req.Value == 4 {
+			return status.Errorf(codes.InvalidArgument, "this call will always fail on four")
+		}
+	}
+	return nil
 }
