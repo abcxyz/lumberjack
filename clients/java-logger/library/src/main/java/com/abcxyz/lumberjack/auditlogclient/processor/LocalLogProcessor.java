bd2ceb16d6125a2aa3d1f528b668c366238bf824
Ryan Serva <raserva@users.noreply.github.com>
2022-03-23 12:25:07 -0700

    feat: Add Support for Local Logging (#199)
    * Adding support for a local logger for java
    
    * Working local log implementation
    
    * update copyright notice
    
    * expand unit tests
    
    * PR feedback
    
    * change to lazy init in getter
    
    * added local as a separate config under backends
    
    * added copyright notice
    
    * remove an unnecessary variable, update references.

---
+++ clients/java-logger/library/src/main/java/com/abcxyz/lumberjack/auditlogclient/processor/LocalLogProcessor.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2022 Lumberjack authors (see AUTHORS file)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.abcxyz.lumberjack.auditlogclient.processor;
+
+import com.abcxyz.lumberjack.auditlogclient.processor.LogProcessor.LogBackend;
+import com.abcxyz.lumberjack.v1alpha1.AuditLogRequest;
+import com.google.inject.Inject;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.extern.java.Log;
+
+/** Logs the {@link AuditLogRequest} using the standard logger. */
+@Log
+@AllArgsConstructor(onConstructor = @__({@Inject}))
+public class LocalLogProcessor implements LogBackend {
+
+  @Override
+  public AuditLogRequest process(AuditLogRequest auditLogRequest) {
+    try {
+      String jsonString = JsonFormat.printer().omittingInsignificantWhitespace().print(auditLogRequest);
+      log.info("Lumberjack log: " + jsonString);
+    } catch (InvalidProtocolBufferException e) {
+      throw new RuntimeException(e);
+    }
+    return auditLogRequest;
+  }
+}
---
+++ clients/java-logger/library/src/test/java/com/abcxyz/lumberjack/auditlogclient/config/BackendContextTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2022 Lumberjack authors (see AUTHORS file)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.abcxyz.lumberjack.auditlogclient.config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class BackendContextTest {
+  @Test
+  public void remoteBackend() throws IOException {
+    ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+    BackendContext backendContext=
+        mapper
+            .readValue(
+                getClass().getClassLoader().getResourceAsStream("backend_remote.yml"),
+                AuditLoggingConfiguration.class)
+            .getBackend();
+
+    assertThat(backendContext.remoteEnabled()).isTrue();
+    assertThat(backendContext.localLoggingEnabled()).isFalse();
+  }
+
+  @Test
+  public void localBackend() throws IOException {
+    ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+    BackendContext backendContext=
+        mapper
+            .readValue(
+                getClass().getClassLoader().getResourceAsStream("backend_local.yml"),
+                AuditLoggingConfiguration.class)
+            .getBackend();
+
+    assertThat(backendContext.remoteEnabled()).isFalse();
+    assertThat(backendContext.localLoggingEnabled()).isTrue();
+  }
+
+  @Test
+  public void bothAsBackend() throws IOException {
+    ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+    BackendContext backendContext=
+        mapper
+            .readValue(
+                getClass().getClassLoader().getResourceAsStream("backend_both.yml"),
+                AuditLoggingConfiguration.class)
+            .getBackend();
+
+    assertThat(backendContext.remoteEnabled()).isTrue();
+    assertThat(backendContext.localLoggingEnabled()).isTrue();
+  }
+}
