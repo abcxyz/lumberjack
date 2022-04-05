75528bc6ef13cc518a08acd50453975611a6fcfb
Mete Yurtoglu <myurtoglu@gmail.com>
2022-04-05 14:49:14 -0700

    Convert LoggingOptions to LoggingOptions.Builder.

--- clients/java-logger/library/src/main/java/com/abcxyz/lumberjack/auditlogclient/modules/CloudLoggingModule.java
+++ clients/java-logger/library/src/main/java/com/abcxyz/lumberjack/auditlogclient/modules/CloudLoggingModule.java
@@ -12,18 +12,15 @@ import com.google.inject.Provides;
 public class CloudLoggingModule extends AbstractModule {
   @Provides
   Logging logging(AuditLoggingConfiguration configuration) {
-    LoggingOptions loggingOptions = LoggingOptions.getDefaultInstance();
+    LoggingOptions.Builder loggingOptionsBuilder = LoggingOptions.newBuilder();
     if (configuration.getBackend().cloudLoggingEnabled()
         && !Strings.isNullOrEmpty(configuration.getBackend().getCloudlogging().getProject())) {
       if (configuration.getBackend().getCloudlogging().useDefaultProject()) {
         throw new IllegalStateException("Cannot set cloud logging project if default is enabled.");
       }
-      loggingOptions =
-          loggingOptions.toBuilder()
-              .setProjectId(configuration.getBackend().getCloudlogging().getProject())
-              .build();
+      loggingOptionsBuilder.setProjectId(configuration.getBackend().getCloudlogging().getProject());
     }
-    Logging logging = loggingOptions.getService();
+    Logging logging = loggingOptionsBuilder.build().getService();
     if (ConfigUtils.shouldFailClose(configuration.getLogMode())) {
       logging.setWriteSynchronicity(Synchronicity.SYNC);
     } else {
