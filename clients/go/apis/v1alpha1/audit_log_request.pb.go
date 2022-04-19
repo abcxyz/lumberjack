9dd48cd49b68f98fd3d9ca4b8daa8f45a011d2d1
Mete Yurtoglu <myurtoglu@gmail.com>
2022-04-18 18:08:06 -0700

    More formatting updates with goimports...

--- clients/go/apis/v1alpha1/audit_log_request.pb.go
+++ clients/go/apis/v1alpha1/audit_log_request.pb.go
@@ -23,14 +23,15 @@
 package v1alpha1
 
 import (
+	reflect "reflect"
+	sync "sync"
+
 	audit "google.golang.org/genproto/googleapis/cloud/audit"
 	v2 "google.golang.org/genproto/googleapis/logging/v2"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	descriptorpb "google.golang.org/protobuf/types/descriptorpb"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
