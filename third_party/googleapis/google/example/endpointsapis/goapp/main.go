963ed5769c95050a5f89abaa1e74521f48bba1e8
Seth Vargo <seth@sethvargo.com>
2022-09-29 13:56:04 -0400

    Update all protos (#316)

--- third_party/googleapis/google/example/endpointsapis/goapp/main.go
+++ third_party/googleapis/google/example/endpointsapis/goapp/main.go
@@ -1,17 +1,3 @@
-// Copyright 2021 Lumberjack authors (see AUTHORS file)
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 // The goapp command implements a simple App Engine app to demonstrate how to
 // use the Service Control API v2 for admission control. For more information,
 // see https://cloud.google.com/service-infrastructure/docs/admission-control.
