b21f2e1b3de0257782c801d90c8a4be7a237a1cf
capri-xiyue <52932582+capri-xiyue@users.noreply.github.com>
2022-12-01 16:43:29 -0800

    feat: Add integration test setup to test ingestion-service-less setup (#340)
    * Add integration test setup to test ingestion-service-less setup
    
    * fixed terraform lint
    
    * fixed terraform
    
    * change github action to add non-ingestion client endpoints
    
    * fixed terraform lint
    
    * fix conflicts of client
    
    * fixed github workflow
    
    * test ci client with golang only
    
    * added java back again
    
    * test to see whether only java cl client passes
    
    * revert to go client only
    
    * added grpc tests back
    
    * fixed terraform lint
    
    * test to see whether java passes
    
    * fixed java lint
    
    * tested with java only
    
    * fixed java
    
    * fixed java lint
    
    * fixed java lint
    
    * enable all tests
    
    * increase integration test running time to avoid flaky
    
    * added code comments
    
    * added code comments
    
    * fixed code comments
    
    * fixed unused imports

--- terraform/modules/ci/main.tf
+++ terraform/modules/ci/main.tf
@@ -15,8 +15,12 @@
  */
 
 locals {
-  client_env_vars = {
+  ingestion_backed_client_env_vars = {
     "AUDIT_CLIENT_BACKEND_REMOTE_ADDRESS" : "${trimprefix(module.server_service.audit_log_server_url, "https://")}:443",
+    "AUDIT_CLIENT_CONDITION_REGEX_PRINCIPAL_INCLUDE" : ".*",
+  }
+  cloudlogging_backend_client_env_vars = {
+    "AUDIT_CLIENT_BACKEND_CLOUDLOGGING_DEFAULT_PROJECT" : "true",
     "AUDIT_CLIENT_CONDITION_REGEX_PRINCIPAL_INCLUDE" : ".*",
   }
   short_sha = substr(var.commit_sha, 0, 7)
@@ -35,10 +39,10 @@ module "server_service" {
   disable_dedicated_sa = true
 }
 
-resource "google_cloud_run_service" "client_services" {
+resource "google_cloud_run_service" "ingestion_backend_client_services" {
   for_each = var.client_images
 
-  name     = "${each.key}-${local.short_sha}"
+  name     = "${each.key}-${local.short_sha}-ingestion"
   project  = var.client_project_id
   location = var.region
 
@@ -54,7 +58,38 @@ resource "google_cloud_run_service" "client_services" {
         }
 
         dynamic "env" {
-          for_each = local.client_env_vars
+          for_each = local.ingestion_backed_client_env_vars
+
+          content {
+            name  = env.key
+            value = env.value
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_service" "cloudlogging_backend_client_services" {
+  for_each = var.client_images
+
+  name     = "${each.key}-${local.short_sha}-cloudlogging"
+  project  = var.client_project_id
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = each.value
+
+        resources {
+          limits = {
+            memory = "1G"
+          }
+        }
+
+        dynamic "env" {
+          for_each = local.cloudlogging_backend_client_env_vars
 
           content {
             name  = env.key
