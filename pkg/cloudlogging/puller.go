5e4ba2c4e03f021d5461e35a8df41d4910f8d01f
Suhong Qin <51539171+sqin2019@users.noreply.github.com>
2023-08-09 11:31:41 -0700

    feat: query cloud logging logs (#434)
    * feat: query cloud logging logs
    
    * add stream pull
    
    * use channel
    
    * use channel
    
    * Revert "use channel"
    
    This reverts commit a1cb47a9f182ca484396e91a4a60a52ef8438c3b.
    
    * Revert "use channel"
    
    This reverts commit c645807b814c51ffe08d697ce74c4b1fafbe3305.
    
    * switch back
    
    * fix nits

---
+++ pkg/cloudlogging/puller.go
@@ -0,0 +1,113 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package cloudlogging pulls lumberjack/data access logs from GCP cloud
+// logging.
+package cloudlogging
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"cloud.google.com/go/logging/apiv2/loggingpb"
+	"github.com/sethvargo/go-retry"
+	"google.golang.org/api/iterator"
+
+	logging "cloud.google.com/go/logging/apiv2"
+)
+
+// Puller pulls log entries of GCP organizations, folders, projects, and
+// billingAccounts.
+type Puller struct {
+	client *logging.Client
+	// Required. Name of a parent resource from which to retrieve log entries:
+	//
+	// *  `projects/[PROJECT_ID]`
+	// *  `organizations/[ORGANIZATION_ID]`
+	// *  `billingAccounts/[BILLING_ACCOUNT_ID]`
+	// *  `folders/[FOLDER_ID]`
+	//
+	// May alternatively be one or more views:
+	//
+	//   - `projects/[PROJECT_ID]/locations/[LOCATION_ID]/buckets/[BUCKET_ID]/views/[VIEW_ID]`
+	//   - `organizations/[ORGANIZATION_ID]/locations/[LOCATION_ID]/buckets/[BUCKET_ID]/views/[VIEW_ID]`
+	//   - `billingAccounts/[BILLING_ACCOUNT_ID]/locations/[LOCATION_ID]/buckets/[BUCKET_ID]/views/[VIEW_ID]`
+	//   - `folders/[FOLDER_ID]/locations/[LOCATION_ID]/buckets/[BUCKET_ID]/views/[VIEW_ID]`
+	resource string
+	// Optional retry backoff strategy, default is 5 attempts with fibonacci
+	// backoff that starts at 500ms.
+	retry retry.Backoff
+}
+
+// Option is the option to set up an log puller.
+type Option func(h *Puller) *Puller
+
+// WithRetry provides retry strategy to the log puller.
+func WithRetry(b retry.Backoff) Option {
+	return func(p *Puller) *Puller {
+		p.retry = b
+		return p
+	}
+}
+
+// NewPuller creates a new Puller with provided clients and options.
+func NewPuller(ctx context.Context, c *logging.Client, resource string, opts ...Option) *Puller {
+	p := &Puller{client: c, resource: resource}
+	for _, opt := range opts {
+		p = opt(p)
+	}
+
+	if p.retry == nil {
+		p.retry = retry.WithMaxRetries(5, retry.NewFibonacci(500*time.Millisecond))
+	}
+	return p
+}
+
+// Pull pulls up to maxCount of log entries for the given log filter.
+func (p *Puller) Pull(ctx context.Context, filter string, maxCount int) ([]*loggingpb.LogEntry, error) {
+	var ls []*loggingpb.LogEntry
+	req := &loggingpb.ListLogEntriesRequest{
+		ResourceNames: []string{p.resource},
+		Filter:        filter,
+		// Set descending time order so that newest logs will be returned.
+		OrderBy: "timestamp desc",
+		// Set a large pagesize to optimize read efficiency, see ref
+		// https://cloud.google.com/logging/docs/reference/api-overview#entries-list.
+		PageSize: 1000,
+	}
+	if err := retry.Do(ctx, p.retry, func(ctx context.Context) error {
+		it := p.client.ListLogEntries(ctx, req)
+		for {
+			// Stop if it reaches maxCount.
+			if len(ls) == maxCount {
+				break
+			}
+
+			l, err := it.Next()
+			if errors.Is(err, iterator.Done) {
+				break
+			}
+			if err != nil {
+				return retry.RetryableError(fmt.Errorf("failed to get next log entry: %w, retrying", err))
+			}
+			ls = append(ls, l)
+		}
+		return nil
+	}); err != nil {
+		return nil, fmt.Errorf("failed to list log entries: %w", err)
+	}
+	return ls, nil
+}
---
+++ pkg/cloudlogging/puller_test.go
@@ -0,0 +1,137 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cloudlogging
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"cloud.google.com/go/logging/apiv2/loggingpb"
+	"github.com/abcxyz/pkg/testutil"
+	"github.com/google/go-cmp/cmp"
+	"github.com/sethvargo/go-retry"
+	"google.golang.org/api/option"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/testing/protocmp"
+
+	logging "cloud.google.com/go/logging/apiv2"
+)
+
+const testResource = "test-resource"
+
+func TestPull(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name          string
+		server        *fakeServer
+		filter        string
+		wantReq       *loggingpb.ListLogEntriesRequest
+		wantResult    []*loggingpb.LogEntry
+		wantErrSubstr string
+	}{
+		{
+			name:   "success",
+			filter: "test-filter",
+			server: &fakeServer{
+				resp: &loggingpb.ListLogEntriesResponse{
+					Entries: []*loggingpb.LogEntry{{LogName: "test"}},
+				},
+			},
+			wantReq: &loggingpb.ListLogEntriesRequest{
+				ResourceNames: []string{testResource},
+				Filter:        "test-filter",
+				OrderBy:       "timestamp desc",
+				PageSize:      1000,
+			},
+			wantResult: []*loggingpb.LogEntry{{LogName: "test"}},
+		},
+		{
+			name:   "failed_to_pull",
+			filter: "",
+			server: &fakeServer{
+				injectedErr: fmt.Errorf("injected error"),
+			},
+			wantReq: &loggingpb.ListLogEntriesRequest{
+				ResourceNames: []string{testResource},
+				OrderBy:       "timestamp desc",
+				PageSize:      1000,
+			},
+			wantErrSubstr: "injected error",
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctx := context.Background()
+			fakeClient := setupFakeClient(t, ctx, tc.server)
+			p := NewPuller(
+				ctx,
+				fakeClient,
+				testResource,
+				WithRetry(retry.WithMaxRetries(0, retry.NewFibonacci(500*time.Millisecond))),
+			)
+			gotResult, gotErr := p.Pull(ctx, tc.filter, 1)
+			if diff := testutil.DiffErrString(gotErr, tc.wantErrSubstr); diff != "" {
+				t.Errorf("Process(%+v) got unexpected error substring: %v", tc.name, diff)
+			}
+
+			if diff := cmp.Diff(tc.wantResult, gotResult, protocmp.Transform()); diff != "" {
+				t.Errorf("Process(%+v) got result diff (-want, +got): %v", tc.name, diff)
+			}
+
+			if diff := cmp.Diff(tc.wantReq, tc.server.req, protocmp.Transform()); diff != "" {
+				t.Errorf("Process(%+v) got request diff (-want, +got): %v", tc.name, diff)
+			}
+		})
+	}
+}
+
+func setupFakeClient(t *testing.T, ctx context.Context, s *fakeServer) *logging.Client {
+	t.Helper()
+
+	// Setup fake server.
+	addr, conn := testutil.FakeGRPCServer(t, func(grpcS *grpc.Server) {
+		loggingpb.RegisterLoggingServiceV2Server(grpcS, s)
+	})
+	t.Cleanup(func() {
+		conn.Close()
+	})
+	// fakeClient := loggingpb.NewLoggingServiceV2Client(conn)
+	fakeClient, err := logging.NewClient(ctx, option.WithGRPCConn(conn))
+	if err != nil {
+		t.Fatalf("creating client for fake at %q: %v", addr, err)
+	}
+	return fakeClient
+}
+
+type fakeServer struct {
+	loggingpb.UnimplementedLoggingServiceV2Server
+
+	req         *loggingpb.ListLogEntriesRequest
+	resp        *loggingpb.ListLogEntriesResponse
+	injectedErr error
+}
+
+func (s *fakeServer) ListLogEntries(ctx context.Context, req *loggingpb.ListLogEntriesRequest) (*loggingpb.ListLogEntriesResponse, error) {
+	s.req = req
+	return s.resp, s.injectedErr
+}
